4f57bc32356f24cc050cf51eeec82a45f71575ea
smartitsoloutions <137606026+smartitsoloutions@users.noreply.github.com>
2023-10-11 10:26:19 +0530

    Update text.java

--- text.java
+++ text.java
@@ -2,9 +2,8 @@ welcoem to smart it soloutions
 
 Offering courses
              AWS AZURE DEVOPS
-<<<<<<< HEAD
 	     new info about project
-=======
+
 	     address location pin code
 
->>>>>>> passwd
+
